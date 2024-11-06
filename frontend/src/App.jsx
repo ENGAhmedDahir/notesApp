334cef34f6d102910ddf4d92e16f84ebc2e5cd3d
ENGAhmedDahir <ahmeddahir381@gmail.com>
2024-11-06 19:51:28 +0300

    somthing changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<SignIn />} />
             <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
+            {/* <Route path="/home" element={<Home />} /> */}
             {/* Add a fallback 404 route */}
             <Route path="*" element={<h2>404: Page Not Found</h2>} />
           </Routes>
