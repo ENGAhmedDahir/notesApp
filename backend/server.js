96e3af3b10c50f6558b82111eca4e81b0a0bca79
ENGAhmedDahir <ahmeddahir381@gmail.com>
2024-11-06 20:41:40 +0300

    secrec updated

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import noteRouter from './routes/notesRouter.js'
 import cookieParser from 'cookie-parser'
 
 import path from  'path';
+import { jwt_Secret } from './config/config.js'
 
 const __dirname = path.resolve();
 
@@ -19,6 +20,8 @@ app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
 })
+
+
 connectDb()
 const port = 3000
 
