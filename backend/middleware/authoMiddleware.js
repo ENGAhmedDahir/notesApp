7f2fc46e7a4e9102abf00aa858bf04cb4d65d61d
ENGAhmedDahir <ahmeddahir381@gmail.com>
2025-02-19 19:07:27 +0300

    added some updates

--- backend/middleware/authoMiddleware.js
+++ backend/middleware/authoMiddleware.js
@@ -16,5 +16,6 @@ export const authenticate =  (req, res, next) =>{
         console.log(error.message)
         
     }
+   
 
 }
--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,8 @@ const userSchema = new Schema({
           }),
           message: 'Password must be at least 8 characters long and include at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 symbol.'
         }
-      }
+      },
+     
 
 },
 {
